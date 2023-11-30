54fa5232bd16818296eb95750b548750f6170095
jacqueline-98 <jacqueline-98@hotmail.com>
2023-11-30 11:59:23 +0100

    extra cycles added

--- GTPexec/prompt_execute_gtp3_5.R
+++ GTPexec/prompt_execute_gtp3_5.R
@@ -49,7 +49,7 @@ output_folder="../results/simpleTest_gtp3_5/"
 myPromptsFile="../scripts/mergen_prompts.Rmd"
 
 ## number of cycles: how many times each prompt should be run
-cycles=5
+cycles=10
 
 ### --- Being tests
 
